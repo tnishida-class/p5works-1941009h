bad188f57d7c0659884f35941473e092123c86e6
1941009h <1941009h@stu.kobe-u.ac.jp>
2020-11-22 15:17:44 +0900

    homework 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("My name is Rui",color(255,0,0),color(255,255,255));
 }
 
-function balloon(t){
+function balloon(t,color1,color2){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  fill(color1);
+  triangle(w+p*2,h+p*2,w-h,h+p*2,w+p+10,h+p+h);
+  fill(color1);
+  rect(0,0, w + p * 2, h + p * 2);
+  fill(color2);
   text(t, p, h + p);
 }
