62553a2ef6539d708d59b995ec8c7f9b47bd58b4
1941009h <1941009h@stu.kobe-u.ac.jp>
2020-11-23 12:48:30 +0900

    homework 4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,16 +1,22 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy,angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 3;
+  vy = 3;
+  angle=0;
 }
 
 function draw(){
   background(160, 192, 255);
+ fill(0);
+ x=x+vx;
+ y=y+vy;
+ angle++;
+ star(x,y,10,angle);
   // BLANK[2] (hint: 作った star 関数を使います)
 
   // 端の処理パターン (1) 反対側から出てくる
@@ -22,8 +28,8 @@ function draw(){
 
 function star(cx, cy, r, angle){
   beginShape();
-  for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+  for(var i = 0; i < 5; i++){
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI+angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
