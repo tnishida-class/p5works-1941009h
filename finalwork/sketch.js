a38af22286c7158c93a1d72143e5cdedde52f4ec
1941009h <1941009h@stu.kobe-u.ac.jp>
2020-11-24 10:13:35 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,5 +1,8 @@
 let count;
 let cycle;
+let a=[];
+let b=[];
+i=0;
 
 function setup(){
   createCanvas(500,500);
@@ -7,11 +10,8 @@ function setup(){
   cycle = 100;
 }
 
-
-
 function draw(){
-  background(160, 192, 255);
-  fill(255,255,0);
+  background(192);
   noStroke();
   count = (count + 1) % cycle;
   let size=count
@@ -22,14 +22,25 @@ function draw(){
     let theta = TWO_PI
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
+  fill(255,255,0);
   star(x,y,size);
   fill(0,128,0);
-  triangle(150,115,60,200,240,200);
-  triangle(150,145,30,240,270,240);
-  triangle(150,175,0,280,300,280);
-  rect(125,280,50,70)
 
+  triangle(150,115,60,220,240,220);
+  triangle(150,145,30,270,270,270);
+  triangle(150,175,0,320,300,320);
+  rect(125,320,50,70);
+  fill(255);
 
+  for(i=0;i<a.length;i++){
+    ellipse(a[i],b[i],10)
+}
+}
+
+function mouseClicked(){
+  a[i]= mouseX;
+  b[i] = mouseY;
+  i++;
 }
 
   function star(cx, cy, r){
